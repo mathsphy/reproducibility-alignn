b3225b6698b6481b127751115e9a78fe320f760e
Kangming Li <27884378+mathsphy@users.noreply.github.com>
2023-10-25 20:17:14 -0400

    Update setup_env.bash

--- setup_env.bash
+++ setup_env.bash
@@ -5,5 +5,4 @@ conda create --name $env_name python=3.10 -y
 conda activate $env_name
 pip install dgl -f https://data.dgl.ai/wheels/cu116/repo.html
 pip install dglgo -f https://data.dgl.ai/wheels-test/repo.html
-pip install alignn 
-
+pip install alignn==2023.5.3
